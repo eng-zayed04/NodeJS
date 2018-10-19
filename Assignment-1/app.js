3933cfe30c9e6dc736fc489abbca038b262d166a
nill_manush <nill_manush@live.com>
2018-10-19 19:08:47 +0600

    Assignment - 1 added

---
+++ Assignment-1/app.js
@@ -0,0 +1,6 @@
+var calculator = require('./calculator.js')
+
+console.log("Result of addition : " + calculator.add(10,5));
+console.log("Result of subtraction : " + calculator.sub(10,5));
+console.log("Result of multiplication : " + calculator.mul(10,5));
+console.log("Result of Devide : " + calculator.div(10,5));
---
+++ Assignment-1/calculator.js
@@ -0,0 +1,18 @@
+function add(f,s)
+{
+    return f+s;
+}
+function sub(f,s)
+{
+    return f-s;
+}
+function mul(f,s)
+{
+    return f*s;
+}
+function div(f,s)
+{
+    return f/s;
+}
+
+module.exports = {add,sub,mul,div};
